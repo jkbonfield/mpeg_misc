88c8643a64ba1d5b46cddd200c1fc400bd95c06c
James Bonfield <jkb@sanger.ac.uk>
2017-03-01 16:43:56 +0000

    Bug fix for decoder name.

--- CE5/loop.sh
+++ CE5/loop.sh
@@ -3,13 +3,13 @@
 in=$1
 
 rm -rf $in.*
-./tokenise_name2 $in
+./tokenise_name2 $in >/dev/null
 ./try_comp.sh $in $in.comp > /dev/null
 ./pack_dir $in.comp $in > $in.comp.pack
 wc -c $in.comp.pack
 ./unpack_dir $in.unpack $in < $in.comp.pack
 ./try_uncomp.sh $in.unpack $in.uncomp 2>/dev/null
-./a.out -d $in.uncomp/$in > $in.new
+./tokenise_name2 -d $in.uncomp/$in > $in.new
 cmp $in $in.new
 
 
